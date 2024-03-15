4dd58387b5f5fd335c9f40b86bc0cc8ff562b3ff
Yuniii-x <melody921203@gmail.com>
2024-03-15 13:50:54 +0800

    113.0315

---
+++ EditTextDemo/app/src/main/java/com/example/edittextdemo/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.edittextdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public  void buttton_Click(View view)
+    {
+        EditText editName = (EditText) findViewById(R.id.editName);
+        TextView txvShow = (TextView) findViewById(R.id.txvShow);
+        String name = editName.getText().toString();
+        txvShow.setText("你的名字：" + name);
+    }
+}
